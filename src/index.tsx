9f6f667f4abd7e04058fc7c3e868543ff477d26f
Ion Korol <ion@korol.dev>
2024-01-01 21:13:35 -0500

    Optimize

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { View } from "react-native";
+import React, { useState, useCallback, useMemo } from "react";
+import { View, StyleSheet } from "react-native";
 import * as utils from "./utils";
-import { Gesture, GestureDetector, GestureStateChangeEvent, GestureTouchEvent } from "react-native-gesture-handler";
+import { Gesture, GestureDetector, GestureTouchEvent } from "react-native-gesture-handler";
 import { IReactNativeJoystickProps } from "./types";
 
 export const ReactNativeJoystick = ({ onStart, onMove, onStop, color = "#000000", radius = 150 }: IReactNativeJoystickProps) => {
@@ -11,43 +11,46 @@ export const ReactNativeJoystick = ({ onStart, onMove, onStop, color = "#000000"
   const [x, setX] = useState(wrapperRadius - nippleRadius);
   const [y, setY] = useState(wrapperRadius - nippleRadius);
 
-  const handleTouchMove = (event: GestureTouchEvent) => {
-    const e = event.changedTouches[0];
-    const fingerX = e.x;
-    const fingerY = e.y;
-    let coordinates = {
-      x: fingerX - nippleRadius,
-      y: fingerY - nippleRadius,
-    };
-
-    const angle = utils.calcAngle({ x: fingerX, y: fingerY }, { x: wrapperRadius, y: wrapperRadius });
-
-    let dist = utils.calcDistance({ x: wrapperRadius, y: wrapperRadius }, { x: fingerX, y: fingerY });
-
-    const force = dist / (nippleRadius * 2);
-
-    dist = Math.min(dist, wrapperRadius);
-    if (dist === wrapperRadius) {
-      coordinates = utils.findCoord({ x: wrapperRadius, y: wrapperRadius }, dist, angle);
-      coordinates = {
-        x: coordinates.x - nippleRadius,
-        y: coordinates.y - nippleRadius,
+  const handleTouchMove = useCallback(
+    (event: GestureTouchEvent) => {
+      const e = event.changedTouches[0];
+      const fingerX = e.x;
+      const fingerY = e.y;
+      let coordinates = {
+        x: fingerX - nippleRadius,
+        y: fingerY - nippleRadius,
       };
-    }
-    setX(coordinates.x);
-    setY(coordinates.y);
 
-    onMove &&
-      onMove({
-        position: coordinates,
-        angle: {
-          radian: utils.degreesToRadians(angle),
-          degree: angle,
-        },
-        force,
-        type: "move",
-      });
-  };
+      const angle = utils.calcAngle({ x: fingerX, y: fingerY }, { x: wrapperRadius, y: wrapperRadius });
+
+      let dist = utils.calcDistance({ x: wrapperRadius, y: wrapperRadius }, { x: fingerX, y: fingerY });
+
+      const force = dist / (nippleRadius * 2);
+
+      dist = Math.min(dist, wrapperRadius);
+      if (dist === wrapperRadius) {
+        coordinates = utils.findCoord({ x: wrapperRadius, y: wrapperRadius }, dist, angle);
+        coordinates = {
+          x: coordinates.x - nippleRadius,
+          y: coordinates.y - nippleRadius,
+        };
+      }
+      setX(coordinates.x);
+      setY(coordinates.y);
+
+      onMove &&
+        onMove({
+          position: coordinates,
+          angle: {
+            radian: utils.degreesToRadians(angle),
+            degree: angle,
+          },
+          force,
+          type: "move",
+        });
+    },
+    [nippleRadius, wrapperRadius]
+  );
 
   const handleTouchEnd = () => {
     setX(wrapperRadius - nippleRadius);
@@ -84,42 +87,37 @@ export const ReactNativeJoystick = ({ onStart, onMove, onStop, color = "#000000"
   };
 
   const panGesture = Gesture.Pan().onStart(handleTouchStart).onEnd(handleTouchEnd).onTouchesMove(handleTouchMove);
+  const styles = useMemo(
+    () =>
+      StyleSheet.create({
+        wrapper: {
+          width: 2 * radius,
+          height: 2 * radius,
+          borderRadius: radius,
+          backgroundColor: `${color}55`,
+          transform: [{ rotateX: "180deg" }],
+        },
+        nipple: {
+          height: 2 * nippleRadius,
+          width: 2 * nippleRadius,
+          borderRadius: nippleRadius,
+          backgroundColor: `${color}bb`,
+          position: "absolute",
+          transform: [
+            {
+              translateX: x,
+            },
+            { translateY: y },
+          ],
+        },
+      }),
+    [radius, color, nippleRadius, x, y]
+  );
 
   return (
     <GestureDetector gesture={panGesture}>
-      <View
-        style={[
-          {
-            width: 2 * radius,
-            height: 2 * radius,
-            borderRadius: radius,
-            backgroundColor: `${color}55`,
-          },
-          {
-            transform: [{ rotateX: "180deg" }],
-          },
-        ]}
-      >
-        <View
-          pointerEvents="none"
-          style={[
-            {
-              height: 2 * nippleRadius,
-              width: 2 * nippleRadius,
-              borderRadius: nippleRadius,
-              backgroundColor: `${color}bb`,
-            },
-            {
-              position: "absolute",
-              transform: [
-                {
-                  translateX: x,
-                },
-                { translateY: y },
-              ],
-            },
-          ]}
-        ></View>
+      <View style={styles.wrapper}>
+        <View pointerEvents="none" style={styles.nipple}></View>
       </View>
     </GestureDetector>
   );
